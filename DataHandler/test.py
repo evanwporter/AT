fcba2adf7a0ad8a5fb1400daecd3e31d15342d84
Xorvall <115374841+evanwporter@users.noreply.github.com>
2025-01-19 12:47:18 -0800

    Moved folders into `src/`

--- DataHandler/test.py
+++
@@ -1,1 +0,0 @@
-from AT import settings
---
+++ src/strategies/lin_regress.py
@@ -0,0 +1,26 @@
+from scipy import stats
+import numpy as np
+
+from strategies import Strategy
+
+
+class LinearRegression(Strategy):
+    def __init__(self, period=30, up_slope=1, down_slope=-1):
+
+        self.period = period
+
+        self.up_slope = up_slope
+        self.down_slope = down_slope
+
+    def on_bar(self):
+
+        data = self.dh.get_latest_data(self.symbol, "Close", self.period)
+
+        if len(data) >= self.period:
+            slope = stats.linregress(np.arange(len(data)), data)[0]
+
+            if slope > self.up_slope:
+                return "LONG"
+
+            # if slope < self.down_slope:
+            #     return "SHORT"
